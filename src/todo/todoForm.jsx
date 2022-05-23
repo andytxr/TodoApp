6cd6dd6ac85b1907954eb940b466674585511ce6
andytxr <andreluiiz.gst@gmail.com>
2022-05-23 17:36:35 -0300

    Implementing shortcuts toadd tasks, search tasks and search cleaning

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,28 +1,45 @@
  import React from "react";
+import { render } from "react-dom";
 
 import Grid from "../template/grid";
 import IconButton from '../template/iconButton';
 
-export default props => (
+export default props => {
 
-    <div role='form' className="todoForm">
+    let keyHandler = (e) => {
+        if(e.key === 'Enter'){
 
-        <Grid cols="12 9 10">
-            <input id= 'description' className="form-control" 
-            placeholder="Adicione uma tarefa" value={props.description} 
-            onChange={props.handleChange}></input>
-        </Grid>
+            e.shiftKey ? props.handleSearch() : props.handleAdd();
 
-        <Grid cols="12 3 2">
+        }else if(e.key === 'Escape'){
+
+            props.handleClear();
+
+        }
+    }
+
+    return(
+
+        <div role='form' className="todoForm">
+
+            <Grid cols="12 9 10">
+                <input id= 'description' className="form-control" 
+                placeholder="Adicione uma tarefa" value={props.description} 
+                onChange={props.handleChange} onKeyUp={keyHandler}></input>
+            </Grid>
+
+            <Grid cols="12 3 2">
+                
+                <IconButton style="primary" icon="plus" onClick={props.handleAdd}> 
+                </IconButton>
+                <IconButton style="info" icon="search" onClick={props.handleSearch}>
+                </IconButton>
+                <IconButton style="default" icon="close" onClick={props.handleClear}></IconButton>
+                
+            </Grid>
             
-            <IconButton style="primary" icon="plus" onClick={props.handleAdd}> 
-            </IconButton>
-            <IconButton style="info" icon="search" onClick={props.handleSearch}>
-            </IconButton>
-            <IconButton style="default" icon="close" onClick={props.handleClear}></IconButton>
-            
-        </Grid>
-            
-    </div>
+        </div>
 
-)
+    )
+
+}
